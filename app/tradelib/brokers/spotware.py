2ce6093f85fa36decad3562849276df4d0b1b919
Ethan <ethanjohol@gmail.com>
2021-02-16 21:25:37 +1100

    added debugging logs

--- app/tradelib/brokers/spotware.py
+++ app/tradelib/brokers/spotware.py
@@ -146,7 +146,7 @@ class Spotware(Broker):
 
 
 	def message(self, payloadType, payload, msgid):
-		# print(f'MSG: ({payloadType}) {payload}')
+		print(f'MSG: ({payloadType}) {payload}')
 
 		# Heartbeat
 		if payloadType == 51:
