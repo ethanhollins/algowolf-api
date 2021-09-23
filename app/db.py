8459fd5e1ff3299101ca0c07bbac81539ef61caa
Ethan <ethanjohol@gmail.com>
2021-09-23 13:06:05 +1000

    Debugging

--- app/db.py
+++ app/db.py
@@ -582,14 +582,16 @@ class Database(object):
 			accounts = dummy_broker.getAllAccounts()
 			dummy_broker.deleteChild()
 
-			print(f'------------------------ [CHECK BROKERS] ---------------------------')
+			print(f'------------------------ [CHECK BROKERS] ---------------------------> {user["brokers"]}')
 			for _id in user['brokers']:
+				print(f"[?????] 1: {_id}")
 				v = user['brokers'][_id]
+				print(f"[?????] 2")
 				v = jwt.decode(
 					v, self.ctrl.app.config['SECRET_KEY'], 
 					algorithms=['HS256']
 				)
-				print(f"{broker_id != _id}, {v.get('broker') == broker_name}, {v.get('accounts') is not None}\n{any([i for i in accounts if i in v['accounts']])}: {[i for i in accounts if i in v['accounts']]}, {accounts}, {v['accounts']}")
+				print(f"[?????] 3 {broker_id != _id}, {v.get('broker') == broker_name}, {v.get('accounts') is not None}\n{any([i for i in accounts if i in v['accounts']])}: {[i for i in accounts if i in v['accounts']]}, {accounts}, {v['accounts']}")
 				if (
 					broker_id != _id and
 					v.get('broker') == broker_name and 
