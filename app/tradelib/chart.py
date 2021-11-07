fda948f774d6973f6bdab944e0d473c988805066
Ethan <ethanjohol@gmail.com>
2021-11-07 21:27:48 +1100

    added mock ticks testing

--- app/tradelib/chart.py
+++ app/tradelib/chart.py
@@ -34,7 +34,7 @@ class Chart(object):
 
 		self.start(await_completion)
 
-		# Thread(target=self._mock_ticks).start()
+		Thread(target=self._mock_ticks).start()
 
 
 	def start(self, await_completion):
@@ -227,7 +227,7 @@ class Chart(object):
 	def _mock_ticks(self):
 		period = tl.period.ONE_MINUTE
 		while self.broker.is_running:
-			time.sleep(30)
+			time.sleep(60)
 			if self.lastTs.get(period):
 				result =[{
 					'broker': 'fxcm',
